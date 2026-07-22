e063bf5b70cfb5209bd5ba01330c813229e8c405
Vaios-S <vaios.sav.dev@gmail.com>
2026-07-22 21:25:18 +0300

    feat(space-page): display data for the selected space

--- src/pages/SpacePage.tsx
+++ src/pages/SpacePage.tsx
@@ -1,10 +1,19 @@
 import { useParams } from "react-router-dom";
+import { spaces } from "../data/spaces";
+
 export default function SpacePage() {
   const { id } = useParams();
 
+  const space = spaces.find((space) => space.id === Number(id));
+
+  if (!space) {
+    return <h1>Space not found</h1>;
+  }
+
   return (
-    <>
-      <h1>Welcome to the Space Page {id}</h1>
-    </>
+    <main>
+      <p>{space.icon}</p>
+      <h1>{space.title}</h1>
+    </main>
   );
 }
