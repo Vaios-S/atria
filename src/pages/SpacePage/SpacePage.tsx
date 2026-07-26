79161164206329512426ffb6b48ca2014f4d27d3
Vaios-S <vaios.sav.dev@gmail.com>
2026-07-26 18:46:26 +0300

    feat(quests): add quest completion toggle

--- src/pages/SpacePage/SpacePage.tsx
+++ src/pages/SpacePage/SpacePage.tsx
@@ -8,6 +8,7 @@ import TodaySection from "../../components/space/TodaySection";
 import CompletedSection from "../../components/space/CompletedSection";
 import SpaceStats from "../../components/space/SpaceStats";
 import { useState } from "react";
+import type { QuestCompletion } from "../../types/questCompletion";
 
 export default function SpacePage() {
   const { id } = useParams();
@@ -15,6 +16,26 @@ export default function SpacePage() {
     useState(mockQuestCompletions);
 
   const space = mockSpaces.find((space) => space.id === id);
+
+  function handleToggleQuest(questId: string) {
+    const completionExists = questCompletions.some(
+      (completion) => completion.questId === questId,
+    );
+
+    if (completionExists) {
+      setQuestCompletions((prev) =>
+        prev.filter((completion) => completion.questId !== questId),
+      );
+    } else {
+      const newCompletion: QuestCompletion = {
+        id: `completed-${questCompletions.length + 1}`,
+        userId: "user-1",
+        questId: `${questId}`,
+        completedAt: new Date().toISOString(),
+      };
+      setQuestCompletions((prev) => [...prev, newCompletion]);
+    }
+  }
 
   if (!space) {
     return <h1>Space not found</h1>;
