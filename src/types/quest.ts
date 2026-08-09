f7e118e6f320162fdcc63af8a8023126062a5e04
Vaios-S <vaios.sav.dev@gmail.com>
2026-08-09 21:33:32 +0300

    feat(quests): support quests without a space

--- src/types/quest.ts
+++ src/types/quest.ts
@@ -3,7 +3,7 @@ export type QuestDifficulty = "easy" | "medium" | "hard";
 export type Quest = {
   id: string;
   userId: string;
-  spaceId: string;
+  spaceId?: string;
   title: string;
   description?: string;
   difficulty: QuestDifficulty;
