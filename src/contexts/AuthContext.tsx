233d32d0bcc5ac55953ab811f16c55efdbc3bfbd
Vaios-S <vaios.sav.dev@gmail.com>
2026-09-01 19:37:04 +0300

    add authentication context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,44 @@
+// React
+import { createContext, useEffect, useState, type ReactNode } from "react";
+
+// Libraries
+import { supabase } from "../lib/supabase";
+
+//Types
+import type { User } from "@supabase/supabase-js";
+
+type AuthContextType = {
+  user: User | null;
+  loading: boolean;
+};
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+
+      setUser(data.user);
+      setLoading(false);
+    };
+
+    getUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 import "./styles/globals.css";
 import "./styles/variables.css";
@@ -11,7 +12,9 @@ import "./styles/typography.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 );
